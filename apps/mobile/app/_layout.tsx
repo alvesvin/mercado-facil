05dc4d379a2c1c852247166d47bfd9dd78da0d33
alvessvin <alvessvin@gmail.com>
2026-03-07 13:27:55 -0300

    posthog

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -2,15 +2,17 @@ import "@/global.css";
 import { ThemeProvider } from "@react-navigation/native";
 import { PortalHost } from "@rn-primitives/portal";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Stack } from "expo-router";
+import { Stack, useGlobalSearchParams, usePathname } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { useColorScheme } from "nativewind";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { api } from "@/lib/api";
 import { auth } from "@/lib/auth";
 import { queryClient } from "@/lib/tanstack-query";
 import { NAV_THEME } from "@/lib/theme";
 import { TRPCProvider } from "@/lib/trpc";
+import { PostHogProvider } from "posthog-react-native";
+import { posthog } from "@/lib/posthog";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -20,21 +22,27 @@ export {
 export default function RootLayout() {
   const { colorScheme } = useColorScheme();
 
+  const pathname = usePathname();
+  const params = useGlobalSearchParams();
+  const lastPathname = useRef<string>(null);
+
   useEffect(() => {
-    // biome-ignore lint/suspicious/noConsole: testing
-    auth.signIn.anonymous().then(console.log).catch(console.error);
-    // router.navigate("/_sitemap");
-  }, []);
+    if (lastPathname.current === pathname) return;
+    lastPathname.current = pathname;
+    posthog.screen(pathname, params);
+  }, [pathname, params]);
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <TRPCProvider queryClient={queryClient} trpcClient={api}>
-        <ThemeProvider value={NAV_THEME[colorScheme ?? "light"]}>
-          <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
-          <Stack screenOptions={{ headerShown: false }} />
-          <PortalHost />
-        </ThemeProvider>
-      </TRPCProvider>
-    </QueryClientProvider>
+    <PostHogProvider client={posthog}>
+      <QueryClientProvider client={queryClient}>
+        <TRPCProvider queryClient={queryClient} trpcClient={api}>
+          <ThemeProvider value={NAV_THEME[colorScheme ?? "light"]}>
+            <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
+            <Stack screenOptions={{ headerShown: false }} />
+            <PortalHost />
+          </ThemeProvider>
+        </TRPCProvider>
+      </QueryClientProvider>
+    </PostHogProvider>
   );
 }
--- apps/mobile/lib/config.ts
+++ apps/mobile/lib/config.ts
@@ -5,10 +5,12 @@ export const config = z
     EXPO_PUBLIC_API_URL: z.url(),
     EXPO_PUBLIC_SUPABASE_URL: z.url(),
     EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY: z.string(),
+    EXPO_PUBLIC_POSTHOG_API_KEY: z.string(),
   })
   .parse({
     // Very important to use process.env.EXPO_URL_* otherwise Expo can't replace it at build time
     EXPO_PUBLIC_API_URL: process.env.EXPO_PUBLIC_API_URL,
     EXPO_PUBLIC_SUPABASE_URL: process.env.EXPO_PUBLIC_SUPABASE_URL,
     EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY: process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY,
+    EXPO_PUBLIC_POSTHOG_API_KEY: process.env.EXPO_PUBLIC_POSTHOG_API_KEY,
   });
